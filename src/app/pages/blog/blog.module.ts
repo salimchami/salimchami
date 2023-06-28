167fa7ac9111da1a4acd79d5ab52297f2ff74b85
Salim CHAMI <it@salimchami.net>
2023-06-28 22:32:11 +0200

    images

--- src/app/pages/blog/blog.module.ts
+++ src/app/pages/blog/blog.module.ts
@@ -3,9 +3,7 @@ import {CommonModule} from '@angular/common';
 
 import {BlogRoutingModule} from './blog-routing.module';
 import {BlogComponent} from './blog.component';
-import {TranslateLoader, TranslateModule, TranslateService, TranslateStore} from "@ngx-translate/core";
-import {HttpClient} from "@angular/common/http";
-import {createTranslateLoader} from "../../app.module";
+import {TranslateModule} from "@ngx-translate/core";
 
 @NgModule({
   declarations: [

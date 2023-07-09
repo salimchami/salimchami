3683fab2829785d09008e25a55bfe6017ad607b0
Salim CHAMI <it@salimchami.net>
2023-07-09 14:35:10 +0200

    images

--- src/app/pages/blog/blog-routing.module.ts
+++ src/app/pages/blog/blog-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlogComponent } from './blog.component';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
 const routes: Routes = [{ path: '', component: BlogComponent }];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(routes),
+  NgbModule],
   exports: [RouterModule]
 })
 export class BlogRoutingModule { }
